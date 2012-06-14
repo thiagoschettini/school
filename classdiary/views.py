0e1ff47a3603b013c8d6939436bbf5c2fc8a4f96
Thiago Schettini <schettinI@gmail.com>
2012-06-13 22:12:40 -0300

    First View
    Created the first view and template for test purposes.

--- classdiary/views.py
+++ classdiary/views.py
@@ -1,1 +1,15 @@
-# Create your views here.
+# Create your views here
+# -*- coding: utf-8 -*-
+
+from django.http import HttpResponseRedirect, HttpResponse
+from django.shortcuts import render_to_response, get_object_or_404
+from django.core.urlresolvers import reverse
+from django.template import Context, loader, RequestContext
+
+from models import *
+from school import settings
+
+def firstView(request):
+	alunos = Aluno.objects.all()
+	turmas = Turma.objects.all()
+	return render_to_response('starter.html', locals(), context_instance=RequestContext(request),)
--- urls.py
+++ urls.py
@@ -6,7 +6,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'school.views.home', name='home'),
+    url(r'^$', 'classdiary.views.firstView', name='home'),
     # url(r'^school/', include('school.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
